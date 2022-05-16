1814b957f693492b2ea8e549ac78f0baa78d9cb2
Ellie Fowler <ellie.elca@gmail.com>
2022-05-16 11:23:50 +0100

    Change query definition a little

--- Data Access Queries/Activity KPIs/NewPlayerStartTimeInUTCTimezone.sql
+++ Data Access Queries/Activity KPIs/NewPlayerStartTimeInUTCTimezone.sql
@@ -3,9 +3,10 @@ WITH firstValues AS (
     SELECT USER_ID,
         MIN(EVENT_TIMESTAMP) AS startTs,
         MIN(EVENT_JSON:timezoneOffset::STRING) AS tzo
-    FROM EVENTS
-    WHERE EVENT_DATE = PLAYER_START_DATE
-    AND EVENT_DATE > CURRENT_DATE - 30
+    FROM account_events
+    WHERE EVENT_JSON:EVENT_DATE::DATE = PLAYER_START_DATE
+    AND EVENT_JSON:EVENT_DATE::DATE > CURRENT_DATE - 30
+    AND ENVIRONMENT_ID = [YOUR TARGET ENVIRONMENT]
     GROUP BY USER_ID
 ),
 
@@ -16,10 +17,10 @@ results AS (
         TIMEADD(minute, 
             CASE 
                 WHEN LEN(tzo) = 5 THEN 
-                    CAST(SUBSTR(tzo, 1, 1)||'1' AS INTEGER) * -- get positive vs negative tzoffset
+                    (CAST(SUBSTR(tzo, 1, 1)||'1' AS INTEGER) * -1) * -- get positive vs negative tzoffset
                     (CAST(SUBSTR(tzo, 2, 2) AS INTEGER) * 60 + CAST(SUBSTR(tzo, 4, 2) AS INTEGER)) 
                 ELSE NULL
-            END, startTs) AS localEventTimestamp,
+            END, startTs) AS UTCTimestamp,
             CASE 
                 WHEN LEN(TZO) = 5 THEN 
                     CAST(SUBSTR(tzo, 1, 1)||'1' AS INTEGER) * -- get positive vs negative tzoffset
@@ -29,6 +30,6 @@ results AS (
    FROM firstValues
 )
 
--- Returns a row per install containing the timestamp the user is first seen on (localEventTimestamp) and the timezoneoffset in minutes.
+-- Returns a row per install containing the timestamp the user is first seen on (UTCTimestamp) and the timezoneoffset in minutes.
 SELECT *
 FROM results
--- Data Access Queries/README.md
+++ Data Access Queries/README.md
@@ -81,7 +81,7 @@ The queries under this category measure how active user have been across a varie
 |Avg Session Length By Session Number|Returns the average session length in minutes, grouped by session number|
 |D28 Churn Vs D28 Rolling Retention|Returns the percentage of players that have churned before Day 28 and the percentage that have been retained until Day 28 per start date|
 |Median and Average Session Lengths|Returns the median and average session length per day|
-|New Players per Hour using Local Player Timezone|Returns the event timestamp for when the player was first seen in their local timezone|
+|New Players Start Time in UTC Timezone|Returns the event timestamp for when the player was first seen in the UTC timezone|
 |New Player Retention Matrix|Returns a matrix which displays how many players who started on a specific date have returned after a certain number of days since they first launched the application|
 |Retention by User Country|Returns D7 retentions for each country that your users come from|
 |Retention Using Min Event Date|Returns D1, D7, D14 and D30 retention using the minimum event date. This prevents sessions which cross into a second day from counting towards D1 retention|
